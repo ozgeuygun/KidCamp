89955967eaf8146731071105b24dde6738c0a435
ozgeuygun <159700383+ozgeuygun@users.noreply.github.com>
2025-05-28 12:42:56 +0300

    Updated

--- KidCamp/DataAccessLayer/EntityFramework/EfAboutDal.cs
+++ KidCamp/DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -8,9 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
-{
-    
-
+{    
     public class EfAboutDal:GenericRepository<About>,IAboutDal
     {
     }
